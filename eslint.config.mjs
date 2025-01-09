99c7e67fe1c004ac0b2285988ef96a1c68629a81
shingari2004 <129884376+shingari2004@users.noreply.github.com>
2025-01-09 19:01:55 +0530

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,9 +12,9 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    rules: {
-      "no-unused-vars": "off", // Disables the rule globally
-    },
+    "rules": {
+      "no-unused-vars": "off"
+    }
   },
 ];
 
